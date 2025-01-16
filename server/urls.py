838ad87f9c2c60839a6122585adfef0ecf10df50
Tomek <lekstomek602@gmail.com>
2025-01-16 17:00:01 +0100

    basic rest api

---
+++ server/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for server project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+
+from server.views import halo, login_username, login_email, signup, test_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path('login_username', login_username),
+    re_path('login_email', login_email),
+    re_path('signup', signup),
+    re_path('test_token', test_token),
+    re_path('helo', halo)
+]
---
+++ server/views.py
@@ -0,0 +1,67 @@
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+from .serializers import UserSerializer
+
+@api_view(['POST'])
+def signup(request):
+    if User.objects.filter(username=request.data['username']).exists():
+        return Response(
+            {"message": "User exists"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        user.set_password(request.data['password'])
+        user.save()
+
+        token = Token.objects.create(user=user)
+
+        user_data = serializer.data
+        user_data.pop('password', None)
+
+        return Response({'token': token.key, 'message': { 'user': user_data}})
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_username(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
+
+@api_view(['POST'])
+def login_email(request):
+    user = get_object_or_404(User,email=request.data['email'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+
+    user_data = serializer.data
+    user_data.pop('password', None)
+
+    return Response({'token': token.key, 'message': { 'user': user_data}})
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response("passed for {}".format(request.user.email))
+
+@api_view(['GET'])
+def halo(request):
+    return Response("helo")
